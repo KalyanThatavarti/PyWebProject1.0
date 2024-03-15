7bb5647d8a2ad32b0543f742c83256d05661ee6e
KalyanThatavarti <kkalyan.thatavarti@gmail.com>
2024-03-15 10:08:30 -0400

    Simple web app for Hello World

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ JOBS = [
 
 @app.route("/")
 def hello_world():
-   return render_template('home.html', jobs="JOBS")
-
+   return render_template('home.html', jobs=JOBS)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
