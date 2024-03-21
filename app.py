0b833fddf1f065defbbf1de50c81658e2db310f9
KalyanThatavarti <kkalyan.thatavarti@gmail.com>
2024-03-21 14:34:42 -0400

    added services

--- app.py
+++ app.py
@@ -35,5 +35,9 @@ JOBS = [
 @app.route("/")
 def hello_world():
    return render_template('home.html', jobs=JOBS)
+@app.route("/services")
+def services():
+   return render_template('services.html', jobs=JOBS)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
