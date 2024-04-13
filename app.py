545f47bf6ba05c97ed9178937583089d6b9b23d7
KalyanThatavarti <kkalyan.thatavarti@gmail.com>
2024-04-13 09:28:15 -0400

    bug fix for templates folder

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db, load_job_from_db
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
