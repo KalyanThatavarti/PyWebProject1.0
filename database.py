6627760e986f5f5f72108ab40495cd8b7a236c74
KalyanThatavarti <kkalyan.thatavarti@gmail.com>
2024-04-27 10:11:18 -0400

    added function add_application_to_db() function to database.py and made changes to app.py

--- database.py
+++ database.py
@@ -28,3 +28,32 @@ def load_job_from_db(id):
         result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {'val': id})
         row = result.fetchone()
         return row._asdict()
+
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+
+        values = {'job_id': job_id,
+                  'first_name': data['First_name'],
+                  'last_name': data['Last_name'],
+                  'email': data['email'],
+                  'mobile': data['mobile'],
+                  'education': data['education'],
+                  'work_experience': data['work_experience'],
+                  'resume_url': data['resume_url']
+                  }
+        query = text(
+            "INSERT INTO applications(job_id, first_name, last_name, email, mobile, education, work_experience, resume_url) VALUES (:job_id, :first_name, :last_name, :email, :mobile, :education, :work_experience, :resume_url)")
+
+        values = {'job_id': job_id,
+                  'first_name': data['First_name'],
+                  'last_name': data['Last_name'],
+                  'email': data['email'],
+                  'mobile': data['mobile'],
+                  'education': data['education'],
+                  'work_experience': data['work_experience'],
+                  'resume_url': data['resume_url']
+                  }
+
+        conn.execute(query, values)
+        conn.commit()
